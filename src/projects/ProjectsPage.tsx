fbe087d6457c1dd42c9f54c3be423b403f5546ec
hwigyeom.kim <hwigyeom.kim@dkbmc.com>
2024-04-17 14:40:35 +0900

    Add Lab3 ~ Lab5

---
+++ src/projects/ProjectsPage.tsx
@@ -0,0 +1,10 @@
+import {MOCK_PROJECTS} from "./MockProjects";
+
+export default function ProjectsPage() {
+    return (
+        <>
+            <h1>Projects</h1>
+            <pre>{JSON.stringify(MOCK_PROJECTS, null, " ")}</pre>
+        </>
+    );
+}
