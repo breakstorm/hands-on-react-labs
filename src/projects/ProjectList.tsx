c37bc3cabd81c31aceacb170e06b86b2b04f55bf
hwigyeom.kim <hwigyeom.kim@dkbmc.com>
2024-04-18 10:28:41 +0900

    Add Lab10 - Create From

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,5 +1,7 @@
 import {Project} from "./Project";
 import ProjectCard from "./ProjectCard";
+import ProjectForm from "./ProjectForm";
+import React from "react";
 
 interface ProjectlistProps {
     projects: Project[];
@@ -9,6 +11,7 @@ export default function ProjectList({projects}: ProjectlistProps) {
     const items = projects.map((project: Project) => (
         <div className="cols-sm" key={project.id}>
             <ProjectCard project={project}/>
+            <ProjectForm />
         </div>
 
     ))
