4666b6df952fe70d74e7a0db1519920be74cc479
hwigyeom.kim <hwigyeom.kim@dkbmc.com>
2024-04-18 17:50:34 +0900

    Add Lab15 - Save logic done

--- src/projects/ProjectForm.tsx
+++ src/projects/ProjectForm.tsx
@@ -1,5 +1,5 @@
 import {Project} from "./Project";
-import {SyntheticEvent} from "react";
+import {SyntheticEvent, useState} from "react";
 
 interface ProjectFormProps {
     project: Project;
@@ -7,36 +7,73 @@ interface ProjectFormProps {
     onCancel: () => void;
 }
 
-export default function ProjectForm({project, onSave, onCancel}:ProjectFormProps):JSX.Element {
-    // const {project, onCancel} = props;
+export default function ProjectForm({
+    project: initialProject,
+    onSave,
+    onCancel}:ProjectFormProps):JSX.Element {
 
+    const [project, setProject] = useState(initialProject);
 
     const handleSubmit = (event: SyntheticEvent) => {
         event.preventDefault();
-        onSave(new Project({name: 'Updated Project'}));
-    }
+        // onSave(new Project({name: 'Updated Project'}));
+        onSave(project);
+    };
+
+    const handleChange = (event: SyntheticEvent) => {
+        // const {type, name, value, checked} = event.target;
+        const target = event.target as HTMLInputElement;
+        const {type, name, value} = target;
+        let checked = type === 'checkbox' ? target.checked : false;
+        // if input type is checkbox use checked
+        // otherwise it's type is text, number etc. so use value
+        let updatedValue: (string | boolean | number) = type === 'checkbox' ? checked : value;
+
+        //if input type is number convert the updatedValue string to a +number
+        if (type === 'number') {
+            updatedValue = Number(updatedValue);
+        }
+        const change = {
+            [name]: updatedValue,
+        };
+
+        let updatedProject: Project;
+        // need to do functional update b/c
+        // the new project state is based on the previous project state
+        // so we can keep the project properties that aren't being edited +like project.id
+        // the spread operator (...) is used to
+        // spread the previous project properties and the new change
+        setProject((p) => {
+            updatedProject = new Project({...p, ...change});
+            return updatedProject;
+        });
+    };
 
     return (
         <form className="input-group vertical" onSubmit={handleSubmit}>
             <label htmlFor="name">Project Name</label>
             <input type="text" name="name" placeholder="enter name"
                 value={project.name}
+                onChange={handleChange}
             />
 
             <label htmlFor="description">Project Description</label>
             <textarea name="description" placeholder="enter description"
                 value={project.description}
+                onChange={handleChange}
             ></textarea>
 
 
             <label htmlFor="budget">Project Budget</label>
             <input type="number" name="budget" placeholder="enter budget"
                 value={project.budget}
+                onChange={handleChange}
             />
 
             <label htmlFor="isActive">Active?</label>
             <input type="checkbox" name="isActive"
                 checked={project.isActive}
+                   onChange={handleChange}
             />
 
             <div className="input-group">
--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,18 +1,24 @@
 import {MOCK_PROJECTS} from "./MockProjects";
 import ProjectList from "./ProjectList";
 import {Project} from "./Project";
+import {useState} from "react";
 
 export default function ProjectsPage() {
+    const [projects, setProjects] = useState(MOCK_PROJECTS);
     const saveProject = (project: Project) => {
         console.log("ProjectsPage ::: saveProject ::: ", project);
-    }
+        let updatedprojects = projects.map((p: Project) => {
+            return p.id === project.id ? project : p;
+        })
+        setProjects(updatedprojects);
+    };
     return (
         <>
             <h1>Projects</h1>
             <ProjectList
-                projects={MOCK_PROJECTS}
+                projects={projects}
                 onSave={saveProject}
             ></ProjectList>
         </>
     );
-}
+};
