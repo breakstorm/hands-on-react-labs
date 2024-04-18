bcaed8b16a196e836f8a3405ff9cf715206cb868
hwigyeom.kim <hwigyeom.kim@dkbmc.com>
2024-04-18 09:45:39 +0900

    Add Lab8 - separate ProjectCard from ProjectList

---
+++ src/projects/ProjectCard.tsx
@@ -0,0 +1,27 @@
+import {Project} from "./Project";
+
+interface ProjectCardProps {
+    project: Project;
+}
+
+function formatDescription(description: string): string {
+    return description.substring(0, 60) + '...';
+}
+
+export default function ProjectCard(props: ProjectCardProps): JSX.Element {
+    const { project } = props;
+    return (
+            <div className="cols-sm">
+                <div className="card">
+                    <img src={project.imageUrl} alt={project.name}/>
+                    <section className="section dark">
+                        <h5 className="strong">
+                            <strong>{project.name}</strong>
+                        </h5>
+                        <p>{formatDescription(project.description)}</p>
+                        <p>Budget : {project.budget.toLocaleString()}</p>
+                    </section>
+                </div>
+            </div>
+    )
+};
--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,29 +1,21 @@
 import {Project} from "./Project";
+import ProjectCard from "./ProjectCard";
 
 interface ProjectlistProps {
     projects: Project[];
 }
 
-export default function ProjectList ({projects}: ProjectlistProps)  {
+export default function ProjectList({projects}: ProjectlistProps) {
+    const items = projects.map((project: Project) => (
+        <div className="cols-sm" key={project.id}>
+            <ProjectCard project={project}/>
+        </div>
+
+    ))
     return (
         <div>
             <div className="row">
-                {projects.map((project: Project) => (
-                    <div key={project.id}>
-                        <div className="cols-sm">
-                            <div className="card">
-                                <img src={project.imageUrl} alt={project.name}/>
-                                <section className="section dark">
-                                    <h5 className="strong">
-                                        <strong>{project.name}</strong>
-                                    </h5>
-                                    <p>{project.description}</p>
-                                    <p>Budget : {project.budget}</p>
-                                </section>
-                            </div>
-                        </div>
-                    </div>
-                ))}
+                {items}
             </div>
         </div>
     );
