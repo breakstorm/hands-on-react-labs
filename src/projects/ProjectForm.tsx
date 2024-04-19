73480e054dcbfec7833a3c16ff38a6696d81e931
hwigyeom.kim <hwigyeom.kim@dkbmc.com>
2024-04-19 11:29:31 +0900

    Add Lab16 - validate logic

--- src/projects/ProjectForm.tsx
+++ src/projects/ProjectForm.tsx
@@ -13,10 +13,22 @@ export default function ProjectForm({
     onCancel}:ProjectFormProps):JSX.Element {
 
     const [project, setProject] = useState(initialProject);
+    const [errors, setErrors] = useState({
+        name: '',
+        description: '',
+        budget: ''
+    })
+
+    // validate requirements
+    // - Name is required.
+    // - Name needs to be at least 3 characters long.
+    // - Description is required.
+    // - Budget must be greater than \$0.
 
     const handleSubmit = (event: SyntheticEvent) => {
         event.preventDefault();
-        // onSave(new Project({name: 'Updated Project'}));
+        if (!isValid()) return;
+
         onSave(project);
     };
 
@@ -47,32 +59,74 @@ export default function ProjectForm({
             updatedProject = new Project({...p, ...change});
             return updatedProject;
         });
+        setErrors(() => validate(updatedProject));
     };
 
+    function validate(project: Project) {
+        let errors = { name: '', description: '', budget: '' };
+
+        if (project.name.length === 0) {
+            errors.name = 'Name is required';
+        }
+        if (project.name.length > 0 && project.name.length < 3) {
+            errors.name = 'Name must be at least 3 characters long';
+        }
+        if (project.description.length === 0) {
+            errors.description = 'Description is required';
+        }
+        if (project.budget === 0) {
+            errors.budget = 'budget must be greater than $0'
+        }
+
+        return errors
+    }
+
+    function isValid() {
+        return (
+            errors.name.length === 0 &&
+            errors.description.length === 0 &&
+            errors.budget.length === 0
+        )
+    }
     return (
         <form className="input-group vertical" onSubmit={handleSubmit}>
             <label htmlFor="name">Project Name</label>
             <input type="text" name="name" placeholder="enter name"
-                value={project.name}
-                onChange={handleChange}
+                   value={project.name}
+                   onChange={handleChange}
             />
+            {errors.name.length > 0 && (
+            <div className="card error">
+                <p>{errors.name}</p>
+            </div>
+            )}
 
             <label htmlFor="description">Project Description</label>
             <textarea name="description" placeholder="enter description"
-                value={project.description}
-                onChange={handleChange}
+                      value={project.description}
+                      onChange={handleChange}
             ></textarea>
+            {errors.description.length > 0 && (
+                <div className="card error">
+                    <p>{errors.description}</p>
+                </div>
+            )}
 
 
             <label htmlFor="budget">Project Budget</label>
             <input type="number" name="budget" placeholder="enter budget"
-                value={project.budget}
-                onChange={handleChange}
+                   value={project.budget}
+                   onChange={handleChange}
             />
+            {errors.budget.length > 0 && (
+                <div className="card error">
+                    <p>{errors.budget}</p>
+                </div>
+            )}
 
             <label htmlFor="isActive">Active?</label>
             <input type="checkbox" name="isActive"
-                checked={project.isActive}
+                   checked={project.isActive}
                    onChange={handleChange}
             />
 
@@ -80,8 +134,9 @@ export default function ProjectForm({
                 <button className="primary bordered medium">Save</button>
                 <span></span>
                 <button type="button" className="bordered medium"
-                    onClick={onCancel}
-                >cancel</button>
+                        onClick={onCancel}
+                >cancel
+                </button>
             </div>
         </form>
     )
